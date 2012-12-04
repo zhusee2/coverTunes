33be43508a761746c3943047bf3597b51a481c40
Zhusee <zhusee2@gmail.com>
2012-12-04 14:16:24 +0800

    Implement expand-close machanism

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,1 +1,48 @@
-$('.playlist').addClass('closed hidden');
+
+$('.playlist').addClass('closed hidden transition transition-height');
+
+$('.cover-picker').on('click', 'a', function(event) {
+  var coverAnchor = this,
+      coverSelected = coverAnchor.parentNode,
+      coverActive = $('.cover-picker .active'),
+      albumSelected = coverAnchor.href.replace('#', ''),
+      indicatorPosition = coverSelected.offsetLeft,
+      playlistHeight = $('.playlist-inner')[0].getBoundingClientRect().height;
+  
+  function togglePlaylistForAlbum(album) {
+    var isExpanding = $('.playlist').hasClass('closed'),
+        targetHeight = isExpanding ? playlistHeight : 0;
+    
+    if (isExpanding) {
+      $('.playlist').removeClass('hidden');
+      $('.playlist-indicator').removeClass('hidden').css('left', indicatorPosition);
+    } else {
+      $('.playlist').on('webkitTransitionEnd', function() {
+        $('.playlist-indicator').addClass('hidden');
+        $('.playlist').addClass('hidden').off('webkitTransitionEnd');
+      })
+    }
+    
+    $(coverSelected).toggleClass('active');
+    $('.playlist').toggleClass('closed expanded').height(targetHeight);
+    
+  }
+  
+  function switchPlaylistToAlbum(album) {
+    $(coverActive).removeClass('active');
+    $(coverSelected).addClass('active');
+    
+    $('.playlist-indicator').css('left', indicatorPosition);
+    
+  }
+  
+  if($(coverActive).length == 0 || coverSelected === coverActive[0]) {
+    togglePlaylistForAlbum(albumSelected);
+  }
+  
+  if($(coverActive).length > 0 && coverSelected !== coverActive[0]) {
+    switchPlaylistToAlbum(albumSelected);
+  }
+  
+  event.preventDefault();
+});
