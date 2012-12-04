a2525e4a5e6baf6b282bf593651cd0868dcaf5fc
Zhusee <zhusee2@gmail.com>
2012-12-04 19:24:11 +0800

    Change SCSS compile settings

--- config.rb
+++ config.rb
@@ -16,10 +16,14 @@ javascripts_dir = "javascripts"
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
 
-
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
 preferred_syntax = :scss
+
+line_comments = false # by Fire.app 
+
+
+output_style = :expanded # by Fire.app 
